ca534ad827ca9f82091168c161b10c91f3546837
Hawty1 <49696027+Hawty1@users.noreply.github.com>
2020-11-23 03:41:10 +0100

    wewe

--- Shopfunctions.js
+++ Shopfunctions.js
@@ -1,3 +1,28 @@
+status = "off";
+
 function activatesearchbar() {
-document.getElementById('searchbar').style.display = 'block'
-}
+  if (status = "off") {
+    console.log(status)
+    document.getElementById('searchbar').style.display = 'block';
+  var  status = "on";
+    console.log (status)
+  } else if (status = "on") {
+    document.getElementById('searchbar').style.display = 'none'
+    console.log("why?")
+  }
+};
+
+
+
+
+/*
+function activatelanguagebar(){
+  document.getElementById('languageicon').onmousedown = 'deactivatelanguagebar()';
+  document.getElementById('languagecontainer').style.display= 'flex';
+  document.getElementById('selectedflag').onmousedown='deactivatelanguagebar()';
+};
+function deactivatelanguagebar(){
+  document.getElementById('languagecontainer').style.display='none'
+
+};
+*/
